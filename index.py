be322a76ea9d2ccd1164ab02f1475af2224da3ca
vidushi-agarwal <vidushiagarwal1997@gmail.com>
2019-04-05 00:42:32 +0530

    sd

--- index.py
+++ index.py
@@ -3,6 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+str_end = "\n Is there anything else you would like to know?"
 def index():
-	str="\n fuck u"
-	return ("<h1>Don't Delete Me</h1>"+str)
+	with open('data_iconicGirls.json') as json_file:
+		data=json.load(json_file)
+		ls=data[str(weekno)][today.strftime("%A")]
+		s='\n'
+		for key in ls.keys():
+			s+="\n" + key + ":\n"
+			s+='\n'.join(ls[key])
+		return ("Your meal for " + str(today.date()) + " is " + s + str_end)
